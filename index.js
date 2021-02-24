80be9d0a09e50a848c9a773a6dedf5b9ad2aa99b
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-24 10:06:58 +0530

    protected route created

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ app.get('/', (req, res) => {
 	res.json({ message: 'Welcome to API' });
 });
 
+// protected route
+app.post('/api/posts', (req, res) => {
+	res.json({ message: 'posts created...' });
+});
+
 const port = 3000;
 app.listen(port, () => console.log(`server started on port : ${port}`));
